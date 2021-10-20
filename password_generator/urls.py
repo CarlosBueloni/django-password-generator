9606fc04aaf773fed8ab83c930d12b2c6d9c81ee
CarlosBueloni <carlosbueloni@gmail.com>
2021-10-20 09:22:31 -0300

    Add Return home link from password page

--- password_generator/urls.py
+++ password_generator/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from generator import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path('password/', views.password, name='password'),
 ]
