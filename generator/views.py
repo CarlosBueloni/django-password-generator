cd6f9bc5eb57e07995ea4c992d1ecbdbe664c720
CarlosBueloni <carlosbueloni@gmail.com>
2021-10-19 22:18:01 -0300

    Create base functionality for generating password

--- generator/views.py
+++ generator/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-
-
+import random
+import string
 # Create your views here.
 def index(request):
     return render(request, 'generator/index.html')
 
 def password(request):
-    return render(request, 'generator/password.html')
+    characters = list(string.ascii_lowercase)
+
+    length = 10
+
+    the_password = ''
+
+    for x in range(length):
+        the_password += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password': the_password})
