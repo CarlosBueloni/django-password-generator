9c671c196445c19b1c0eed62726c820ecc8e3be7
CarlosBueloni <carlosbueloni@gmail.com>
2021-10-20 09:17:34 -0300

    Add password generation logic

--- generator/views.py
+++ generator/views.py
@@ -7,9 +7,17 @@ def index(request):
 
 def password(request):
     characters = list(string.ascii_lowercase)
+    is_uppercase = request.GET.get('uppercase', False)
+    is_special = request.GET.get('special', False)
+    is_numbers = request.GET.get('numbers', False)
 
-    length = 10
+    if is_uppercase:
+        characters.extend(string.ascii_uppercase)
+    if is_special:
+        characters.extend(list('"!@#$%^&*()-+?_=,<>/"'))
+    if is_numbers:
+        characters.extend(list('1234567890'))
 
+    length = int(request.GET.get('length', 12))
     the_password = ''.join(random.choice(characters) for _ in range(length))
-
     return render(request, 'generator/password.html', {'password': the_password})
